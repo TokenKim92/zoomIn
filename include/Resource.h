c7a9cdfccbb74cd3a91c6583889e16e1bcfd50cb
Token Kim <tokenkim92@gmail.com>
2024-01-01 20:27:16 +0100

    feature: change icon

--- include/Resource.h
+++ include/Resource.h
@@ -2,17 +2,16 @@
 // Microsoft Visual C++ generated include file.
 // Used by AppTemplate.rc
 
-#define IDS_APP_TITLE			103
+#define IDS_APP_TITLE			10
 
 #define IDR_MAINFRAME			128
-#define IDD_APPTEMPLATE_DIALOG	    102
+#define IDD_APPTEMPLATE_DIALOG	102
 #define IDD_ABOUTBOX			103
 #define IDM_ABOUT				104
 #define IDM_EXIT				105
-#define IDI_APPTEMPLATE			    107
+#define IDI_APPTEMPLATE			107
 #define IDI_SMALL				108
-#define IDC_APPTEMPLATE			    109
-#define IDC_MYICON				2
+#define IDC_APPTEMPLATE			109
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
 #endif
