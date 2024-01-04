be486f80d67e3c662587a32984be0a699199e3f0
Token Kim <tokenkim92@gmail.com>
2024-01-04 21:41:42 +0100

    fix: set window position on execution

--- src/zoomIn.cpp
+++ src/zoomIn.cpp
@@ -14,9 +14,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE ah_instance, _In_opt_ HINSTANCE ah_notUseIn
 {
     ApplicationCore appCore(ah_instance);
     if (S_OK == appCore.Create()) {
+        const int centerPosX = ::GetSystemMetrics(SM_CXSCREEN) / 2;
+        const int centerPosY = ::GetSystemMetrics(SM_CYSCREEN) / 2;
+        const int size = 256;
+
         zoomInDialog dialog;
         dialog.SetExtendStyle(WS_EX_TOPMOST);
-        return dialog.Create(256, 256);
+        return dialog.Create(size, size, centerPosX - size / 2, centerPosY - size / 2);
     }
 
     return 0;
